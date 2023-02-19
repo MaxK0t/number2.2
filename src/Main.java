4110c1b67296750046fc25ac2f24e4abd323928b
Жиза <grekovgoga@gmail.com>
2023-02-19 23:43:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  
+    public static void main(String[] args) {
+        Result bbb = new Result();
+        bbb.setNum(23);
+        bbb.getNum();
+    }
+}
---
+++ src/Result.java
@@ -0,0 +1,17 @@
+public class Result {
+    private int num;
+
+    public void setNum(int num){
+        this.num = num;
+    }
+
+    public int getNum(){
+        int c = num + 7;
+        if (c % 2 == 0){
+            System.out.println("Digit ends on 3");
+        } else {
+            System.out.println("Digit ends ain't on 3");
+        }
+        return num;
+    }
+}
